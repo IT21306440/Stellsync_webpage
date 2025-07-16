c3de4fa29e7f0a97ed6a1fa38ee13b65410c5b41
IT21306440 <it21306440@mt.sliit.lk>
2025-07-16 15:55:12 +0530

    updating

--- stellsync_chatbot/app.py
+++ stellsync_chatbot/app.py
@@ -605,7 +605,7 @@ def serve_index():
     return send_from_directory('static', 'index.html')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#if __name__ == "__main__":
+#    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
